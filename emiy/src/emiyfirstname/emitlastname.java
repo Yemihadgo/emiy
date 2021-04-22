fc4e4458df84e2aec697cce6200f75bd1b82c80b
Yemihadgo <hadgoyemsrach21@gmail.com>
2021-04-22 03:03:03 -0400

    info updated

---
+++ emiy/src/emiyfirstname/emitlastname.java
@@ -0,0 +1,10 @@
+package emiyfirstname;
+
+public class emitlastname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Information");
+	}
+
+}
